568d4ac70d0a1969da42bbd3407afc9115638444
Oliver <dombovarioliver03@gmail.com>
2026-05-06 16:10:23 +0200

    add: frontend init

---
+++ Frontend/diploma-fit-frontend/src/features/home/pages/HomePage.jsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main className="container mt-5">
+      <h1>DiplomaFit</h1>
+      <p>A frontend alkalmazás sikeresen elindult.</p>
+    </main>
+  );
+}
