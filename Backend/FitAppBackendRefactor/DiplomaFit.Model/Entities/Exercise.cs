7f71326fb18480c2cc6bdf5f283cf719b2f1f4a3
Oliver <dombovarioliver03@gmail.com>
2026-05-10 04:12:12 +0200

    videoUrl-s added to sql, new folder (wwwroot)

--- Backend/FitAppBackendRefactor/DiplomaFit.Model/Entities/Exercise.cs
+++ Backend/FitAppBackendRefactor/DiplomaFit.Model/Entities/Exercise.cs
@@ -21,7 +21,7 @@ namespace DiplomaFit.Model.Entities
         public string NameEn { get; set; } = null!;
 
         [StringLength(250)]
-        public string VideoUrl { get; set; } = null!;
+        public string VideoUrl { get; set; } = string.Empty;
 
         [StringLength(250)]
         public string PrimaryMuscleGroup { get; set; } = null!;      // pl. chest, back, legs, shoulders
